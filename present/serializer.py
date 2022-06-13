f99bbd6544c70c54090448178cee9892cd438ce5
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-13 08:10:36 +0300

    present/apps:created clas profileserializer and projectserializer

---
+++ present/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Project,Profile
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo', 'bio', 'user')
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'image', 'description', 'url', 'posted_date', 'user','rate')
